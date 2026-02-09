b509cf11c122f887332e65b8786f0d2b9813b06e
dy3722 <dy3722@bs.amalnet.k12.il>
2026-02-09 23:52:19 +0200

    end of all!

--- app/src/main/java/com/example/moodtrackerfragments/HappyFragment.java
+++ app/src/main/java/com/example/moodtrackerfragments/HappyFragment.java
@@ -7,11 +7,30 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class HappyFragment extends Fragment {
+    private TextView tv;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // השורה הזו מחברת בין קובץ ה-Java לקובץ ה-XML של הפרגמנט
-        return inflater.inflate(R.layout.fragment_happy, container, false);
+        View view = inflater.inflate(R.layout.fragment_happy, container, false);
+        tv = view.findViewById(R.id.textViewH);
+        return view;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (getActivity() != null) {
+            EditText et = getActivity().findViewById(R.id.et);
+            if (et != null && tv != null) {
+                tv.setText("היי " + et.getText().toString() + "!");
+            }
+        }
+    }
+
+    public void updateName(String name) {
+        if (tv != null) tv.setText("היי " + name + "!");
     }
 }
--- app/src/main/java/com/example/moodtrackerfragments/MainActivity.java
+++ app/src/main/java/com/example/moodtrackerfragments/MainActivity.java
@@ -1,18 +1,48 @@
 package com.example.moodtrackerfragments;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et = findViewById(R.id.et);
+
 
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        et.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                Fragment currentFragment = getSupportFragmentManager().findFragmentById(R.id.fragment_container);
+                String name = s.toString();
+
+                if (currentFragment instanceof HappyFragment) {
+                    ((HappyFragment) currentFragment).updateName(name);
+                } else if (currentFragment instanceof NeutralFragment) {
+                    ((NeutralFragment) currentFragment).updateName(name);
+                } else if (currentFragment instanceof SadFragment) {
+                    ((SadFragment) currentFragment).updateName(name);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
 
         // הגדרת מאזין ללחיצות בתפריט
         bottomNav.setOnItemSelectedListener(item -> {
@@ -22,15 +52,19 @@ public class MainActivity extends AppCompatActivity {
             // בדיקה איזה כפתור נלחץ
             if (id == R.id.nav_happy) {
                 selectedFragment = new HappyFragment();
+                bottomNav.setBackgroundColor(Color.rgb(250, 234, 5));
             } else if (id == R.id.nav_neutral) {
                 selectedFragment = new NeutralFragment();
+                bottomNav.setBackgroundColor(Color.rgb(166, 168, 173));
             } else if (id == R.id.nav_sad) {
                 selectedFragment = new SadFragment();
+                bottomNav.setBackgroundColor(Color.rgb(66, 115, 237));
             }
 
             // החלפת הפרגמנט בפועל
             if (selectedFragment != null) {
                 getSupportFragmentManager().beginTransaction()
+                        .setCustomAnimations(R.anim.slide_in_left, R.anim.slide_out_right)
                         .replace(R.id.fragment_container, selectedFragment)
                         .commit();
             }
@@ -39,6 +73,7 @@ public class MainActivity extends AppCompatActivity {
 
         // הצגת פרגמנט ברירת מחדל בכניסה לאפליקציה
         if (savedInstanceState == null) {
+            bottomNav.setBackgroundColor(Color.rgb(250, 234, 5));
             getSupportFragmentManager().beginTransaction()
                     .replace(R.id.fragment_container, new HappyFragment())
                     .commit();
